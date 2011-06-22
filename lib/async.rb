b84c378fcd1ffead96f65a1fd135792423310468
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2011-06-22 00:46:45 -0600

    Refactor async.rb. Closes #34

--- lib/async.rb
+++ lib/async.rb
@@ -13,89 +13,97 @@ class Async
 
   # TODO: Cleanup
   def initialize
-    tmp = Dir.tmpdir
-    redis = Redis.new
-
-    extension_swap = ->(path, after) do
-      path.gsub(/[^.]+$/, after)
-    end
-
-    # Cleanup files after they are no longer needed
-    cleanup_queue = GirlFriday::WorkQueue.new(:cleanup) do |directory|
-      FileUtils.rm_rf(directory)
-    end
-
+    @tmp = Dir.tmpdir
+    @redis = Redis.new
     # Send errors to Loggly
-    error_queue = @error = GirlFriday::WorkQueue.new(:error) do |message|
-      error, directory = message.values_at(:error, :working)
-      cleanup_queue << directory
-      Loggly.error(error)
-    end
-
+    @error = GirlFriday::WorkQueue.new(:error, &method(:error_handler))
+    # Cleanup files after they are no longer needed
+    @cleanup = GirlFriday::WorkQueue.new(:cleanup, &method(:cleanup))
     # Send emails
-    email_queue = SafeQueue.build(:email) do |message|
-      key, email, url, title, mobi = message.values_at(:key, :email, :url, :title, :mobi)
-      User.mail(email, title, url, mobi)
-      User.notify(redis, key, 'All done! Grab your Kindle and hang tight!')
-    end
-
+    @emails = SafeQueue.build(:email, &method(:send_email))
     # Run kindlegen
-    kindlegen_queue = SafeQueue.build(:kindlegen) do |message|
-      key, html, title, working, epub, url = message.values_at(:key, :html, :title, :working, :epub, :url)
-      mobi = extension_swap.call(epub, 'mobi')
-      pid = Spoon.spawnp('kindlegen', epub)
-      _, status = Process.waitpid2(pid)
-      # Will probably run with warnings, and return 1 instead
-      if File.exists?(mobi)
-        User.notify(redis, key, 'Third stage finished...')
-        message.merge!(mobi: mobi)
-        email_queue << message
-      else
-        error_queue << { error: "kindlegen blew up on #{url}", working: working }
-        User.notify(redis, key, 'Third stage failed. Developer notified.')
-      end
-    end
-
-    write_epub_xml = proc do |epub, title, author|
-      xml = extension_swap.call(epub, 'xml')
-      File.open(xml, 'w') do |f|
-        f.write("<dc:title>#{title}</dc:title>\n<dc:creator>#{author}</dc:creator>\n")
-      end
-      xml
-    end
-
+    @kindlegen = SafeQueue.build(:kindlegen, &method(:run_kindlegen))
     # Run pandoc
-    pandoc_queue = SafeQueue.build(:pandoc) do |message|
-      key, html, title, author, working, url = message.values_at(:key, :html, :title, :author, :working, :url)
-      epub = extension_swap.call(html, 'epub')
-      xml = write_epub_xml.call(epub, title, author)
-      pid = Spoon.spawnp('pandoc', '--epub-metadata', xml, '-o', epub, html)
-      _, status = Process.waitpid2(pid)
-      if status.success?
-        User.notify(redis, key, 'Second stage finished...')
-        message.merge!(epub: epub)
-        kindlegen_queue << message
-      else
-        error_queue << { error: "pandoc blew up on #{url}", working: working }
-        User.notify(redis, key, 'Second stage failed. Developer notified.')
-      end
-    end
-
+    @pandoc = SafeQueue.build(:pandoc, &method(:run_pandoc))
     # Run extraction
-    @extractor = SafeQueue.build(:extractor) do |message|
-      key, url = message.values_at(:key, :url)
-      working = File.join(tmp, key)
-      ex = Extractor::ReadabilityApi.new(url, working)
-      begin
-        outfile, title, author = ex.extract!
-        User.notify(redis, key, 'First stage finished...')
-        message.merge!(html: outfile, title: title, author: author, working: working)
-        pandoc_queue << message
-      rescue Exception => boom
-        HoptoadNotifier.notify_or_ignore(boom)
-        error_queue << { error: "Failed extracting URL: #{url}", working: working }
-        User.notify(redis, key, 'Failed extracting this page. Developer notified.')
-      end
+    @extractor = SafeQueue.build(:extractor, &method(:run_extractor))
+  end
+
+private
+
+  def extension_swap(path, after)
+    path.gsub(/[^.]+$/, after)
+  end
+
+  def write_epub_xml(epub, title, author)
+    xml = extension_swap(epub, 'xml')
+    File.open(xml, 'w') do |f|
+      f.write("<dc:title>#{title}</dc:title>\n<dc:creator>#{author}</dc:creator>\n")
+    end
+    xml
+  end
+
+  def cleanup(directory)
+    FileUtils.rm_rf(directory) if File.exists?(directory)
+  end
+
+  def error_handler(message)
+    error, directory = message.values_at(:error, :working)
+    @cleanup << directory
+    Loggly.error(error)
+  end
+
+  def send_email(message)
+    key, email, url, title, mobi = message.values_at(:key, :email, :url, :title, :mobi)
+    User.mail(email, title, url, mobi)
+    User.notify(@redis, key, 'All done! Grab your Kindle and hang tight!')
+  end
+
+  def run_kindlegen(message)
+    key, html, title, working, epub, url = message.values_at(:key, :html, :title, :working, :epub, :url)
+    mobi = extension_swap(epub, 'mobi')
+    pid = Spoon.spawnp('kindlegen', epub)
+    _, status = Process.waitpid2(pid)
+    # Will probably run with warnings, and return 1 instead
+    if File.exists?(mobi)
+      User.notify(@redis, key, 'Third stage finished...')
+      message.merge!(mobi: mobi)
+      @emails << message
+    else
+      @error << { error: "kindlegen blew up on #{url}", working: working }
+      User.notify(@redis, key, 'Third stage failed. Developer notified.')
+    end
+  end
+
+  def run_pandoc(message)
+    key, html, title, author, working, url = message.values_at(:key, :html, :title, :author, :working, :url)
+    epub = extension_swap(html, 'epub')
+    xml = write_epub_xml(epub, title, author)
+    pid = Spoon.spawnp('pandoc', '--epub-metadata', xml, '-o', epub, html)
+    _, status = Process.waitpid2(pid)
+    if status.success?
+      User.notify(@redis, key, 'Second stage finished...')
+      message.merge!(epub: epub)
+      @kindlegen << message
+    else
+      @error << { error: "pandoc blew up on #{url}", working: working }
+      User.notify(@redis, key, 'Second stage failed. Developer notified.')
+    end
+  end
+
+  def run_extractor(message)
+    key, url = message.values_at(:key, :url)
+    working = File.join(@tmp, key)
+    ex = Extractor::ReadabilityApi.new(url, working)
+    begin
+      outfile, title, author = ex.extract!
+      User.notify(@redis, key, 'First stage finished...')
+      message.merge!(html: outfile, title: title, author: author, working: working)
+      @pandoc << message
+    rescue Exception => boom
+      HoptoadNotifier.notify_or_ignore(boom)
+      @error << { error: "Failed extracting URL: #{url}", working: working }
+      User.notify(@redis, key, 'Failed extracting this page. Developer notified.')
     end
   end
 end
