6434beaea68f7773eec2c9fa2c9d825d6c7766e5
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2011-07-10 16:37:05 -0600

    cleanup in the SafeQueue

--- lib/safe_queue.rb
+++ lib/safe_queue.rb
@@ -11,6 +11,9 @@ module SafeQueue
         rescue Exception => boom
           HoptoadNotifier.notify_or_ignore(boom)
           User.notify(RedisClient, message[:key], "Processing failed; developer notified. Try remaking the bookmarklet.")
+          if working = message[:working]
+            FileUtils.rm_rf(working) if File.exists?(working)
+          end
           Loggly.error("Caught error in queue #{queue}: #{boom.message}")
         end
       end
