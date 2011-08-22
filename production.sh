d69008f6c298ae350ba1cc4c78455a2cc531afae
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2011-08-22 11:24:07 -0600

    Don't need that much memory.

--- production.sh
+++ production.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export RACK_ENV=production
-export JRUBY_OPTS="--1.9 --server --headless -J-Djruby.reify.classes=true -J-Xmx1024m -J-XX:+UseConcMarkSweepGC -J-XX:+UseParNewGC -J-XX:+HeapDumpOnOutOfMemoryError"
+export JRUBY_OPTS="--1.9 --server --headless -J-Djruby.reify.classes=true -J-Xmx768m -J-XX:+UseConcMarkSweepGC -J-XX:+UseParNewGC -J-XX:+HeapDumpOnOutOfMemoryError"
 export PATH=/opt/jruby/bin:$PATH
 yui-compressor public/bookmarklet.js > public/bookmarklet.min.js
 jruby -S bundle exec trinidad --env production --config
